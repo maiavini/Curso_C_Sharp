ce25b54f182d022720547ed6ebe7b3d1a6133b94
Vinicius Maia <maiavini@gmail.com>
2021-03-06 17:01:32 +0000

    Classes e Estruturas Importantes
    Aprendizado de Time Span, Date Time, Color, Font, Enviroment, Application

---
+++ Classes e Estruturas Importantes/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Classe_Estruturas_Importantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.MessBox = new System.Windows.Forms.Button();
+            this.btnaleatorio = new System.Windows.Forms.Button();
+            this.btnTimeSpan = new System.Windows.Forms.Button();
+            this.btnDTime = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnEnv = new System.Windows.Forms.Button();
+            this.btnApl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(30, 27);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(367, 114);
+            this.lblresultado.TabIndex = 1;
+            this.lblresultado.Text = "label1";
+            // 
+            // MessBox
+            // 
+            this.MessBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MessBox.Location = new System.Drawing.Point(30, 265);
+            this.MessBox.Name = "MessBox";
+            this.MessBox.Size = new System.Drawing.Size(102, 50);
+            this.MessBox.TabIndex = 0;
+            this.MessBox.Text = "Message Box";
+            this.MessBox.UseVisualStyleBackColor = true;
+            this.MessBox.Click += new System.EventHandler(this.MessBox_Click);
+            // 
+            // btnaleatorio
+            // 
+            this.btnaleatorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnaleatorio.Location = new System.Drawing.Point(148, 265);
+            this.btnaleatorio.Name = "btnaleatorio";
+            this.btnaleatorio.Size = new System.Drawing.Size(102, 50);
+            this.btnaleatorio.TabIndex = 2;
+            this.btnaleatorio.Text = "Aleatorio";
+            this.btnaleatorio.UseVisualStyleBackColor = true;
+            this.btnaleatorio.Click += new System.EventHandler(this.btnaleatorio_Click);
+            // 
+            // btnTimeSpan
+            // 
+            this.btnTimeSpan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimeSpan.Location = new System.Drawing.Point(271, 265);
+            this.btnTimeSpan.Name = "btnTimeSpan";
+            this.btnTimeSpan.Size = new System.Drawing.Size(102, 50);
+            this.btnTimeSpan.TabIndex = 3;
+            this.btnTimeSpan.Text = "Time Span";
+            this.btnTimeSpan.UseVisualStyleBackColor = true;
+            this.btnTimeSpan.Click += new System.EventHandler(this.btnTimeSpan_Click);
+            // 
+            // btnDTime
+            // 
+            this.btnDTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDTime.Location = new System.Drawing.Point(388, 265);
+            this.btnDTime.Name = "btnDTime";
+            this.btnDTime.Size = new System.Drawing.Size(102, 50);
+            this.btnDTime.TabIndex = 4;
+            this.btnDTime.Text = "Date Time";
+            this.btnDTime.UseVisualStyleBackColor = true;
+            this.btnDTime.Click += new System.EventHandler(this.btnDTime_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColor.Location = new System.Drawing.Point(30, 340);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(102, 50);
+            this.btnColor.TabIndex = 5;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFont.Location = new System.Drawing.Point(148, 340);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(102, 50);
+            this.btnFont.TabIndex = 6;
+            this.btnFont.Text = "Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // btnEnv
+            // 
+            this.btnEnv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnv.Location = new System.Drawing.Point(271, 340);
+            this.btnEnv.Name = "btnEnv";
+            this.btnEnv.Size = new System.Drawing.Size(102, 50);
+            this.btnEnv.TabIndex = 7;
+            this.btnEnv.Text = "Enviroment";
+            this.btnEnv.UseVisualStyleBackColor = true;
+            this.btnEnv.Click += new System.EventHandler(this.btnEnv_Click);
+            // 
+            // btnApl
+            // 
+            this.btnApl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApl.Location = new System.Drawing.Point(388, 340);
+            this.btnApl.Name = "btnApl";
+            this.btnApl.Size = new System.Drawing.Size(102, 50);
+            this.btnApl.TabIndex = 8;
+            this.btnApl.Text = "Aplication";
+            this.btnApl.UseVisualStyleBackColor = true;
+            this.btnApl.Click += new System.EventHandler(this.btnApl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 402);
+            this.Controls.Add(this.btnApl);
+            this.Controls.Add(this.btnEnv);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnDTime);
+            this.Controls.Add(this.btnTimeSpan);
+            this.Controls.Add(this.btnaleatorio);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.MessBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Button MessBox;
+        private System.Windows.Forms.Button btnaleatorio;
+        private System.Windows.Forms.Button btnTimeSpan;
+        private System.Windows.Forms.Button btnDTime;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnEnv;
+        private System.Windows.Forms.Button btnApl;
+    }
+}
+
---
+++ Classes e Estruturas Importantes/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classe_Estruturas_Importantes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MessBox_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Menssagem de Texto", "Titulo da Menssagem");
+            //MessageBox.Show("Menssagem de Texto", "Titulo da Menssagem", MessageBoxButtons.YesNo);
+
+            //DialogResult res = MessageBox.Show("Menssagem de Texto", "Titulo da Menssagem", MessageBoxButtons.YesNoCancel);
+
+            //if (res == DialogResult.Yes)
+            //{
+            //    lblresultado.Text = "Clicou em Yes";
+            //} else if( res == DialogResult.No)
+            //{
+            //    lblresultado.Text = "Clicou em No";
+            //}
+            //else
+            //{
+            //    lblresultado.Text = "Clicou em Cancelar";
+            //}
+
+            MessageBox.Show("Menssagem de Texto", "Titulo da Menssagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        
+        }
+
+        private void btnaleatorio_Click(object sender, EventArgs e)
+        {   
+            //GERAM NUMEROS ALEATORIOS
+            Random r = new Random(DateTime.Now.Millisecond);//Esse parametro serve para que possamos passar um padrão para a busca que é feita através de calculos
+
+            int valor =  r.Next(100);
+            double valor2 = r.NextDouble();
+
+            lblresultado.Text = "Numero : " + valor2 + " Numero : " + valor;
+        }
+
+        private void btnTimeSpan_Click(object sender, EventArgs e)
+        {
+            //MOSTRA INTERVALOS DE TEMPO
+            //lblresultado.Text = TimeSpan.FromMinutes(120).ToString();
+
+            //lblresultado.Text = TimeSpan.FromDays(7.50).ToString();
+
+            //lblresultado.Text = TimeSpan.FromTicks(100000).ToString();// Menor intervalo de tempo no Time Span
+
+            TimeSpan inicio = new TimeSpan(3, 40, 0);
+
+            TimeSpan fim = new TimeSpan(6, 50, 0);
+
+            TimeSpan resposta = fim - inicio;
+
+            lblresultado.Text = resposta.TotalHours.ToString();
+
+
+
+        }
+
+        private void btnDTime_Click(object sender, EventArgs e)
+        {
+            //Representa um determinado tempo no espaço
+
+            //lblresultado.Text = DateTime.Now.;
+            //lblresultado.Text = DateTime.Today.ToString();
+            //lblresultado.Text = DateTime.DaysInMonth(2021,6).ToString();
+            //lblresultado.Text = DateTime.IsLeapYear(2027).ToString();
+            //lblresultado.Text = DateTime.Now.ToLongDateString();
+            //lblresultado.Text = DateTime.Now.ToShortDateString();
+            //lblresultado.Text = DateTime.Now.ToLongTimeString();
+            //lblresultado.Text = DateTime.Now.ToShortTimeString();
+            //lblresultado.Text = DateTime.Now.ToUniversalTime().ToString();
+            //lblresultado.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss" );
+
+            DateTime date = new DateTime(1985, 01, 10, 14, 35, 30);
+
+            //lblresultado.Text = date.AddYears(2).ToString();
+
+            //TimeSpan tempo = new TimeSpan(5, 10, 5, 20);
+            //lblresultado.Text = date.Add(tempo).ToString();
+
+            //lblresultado.Text = date.DayOfWeek.ToString();
+
+            lblresultado.Text = date.DayOfYear.ToString();
+
+
+
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            Color azul = Color.Blue;
+            Color branco = Color.White;
+
+            //Color cor1 = Color.FromArgb(150, Color.Aquamarine);
+            //Color cor2 = Color.FromArgb(215, 17, 56, 78);
+
+            Color cor1 = Color.FromArgb(100, 50, 135);
+            Color cor2 = Color.FromKnownColor(KnownColor.LimeGreen); //Cores conhecidas do sistema
+            Color cor3 = Color.FromName("Orange");
+
+            lblresultado.BackColor = cor3;
+            lblresultado.ForeColor = cor2;
+
+            Color cor4 = lblresultado.BackColor;
+
+            btnColor.BackColor = cor4;
+            btnColor.ForeColor = cor1;
+
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            Font letra = new Font("Comic Sans MS", 15, FontStyle.Italic | FontStyle.Bold, GraphicsUnit.Pixel);
+
+            Font letra2 = new Font(FontFamily.GenericMonospace, 28, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblresultado.Text = "Bem-Vindo a Portugal";
+
+            lblresultado.Font = letra2;
+        }
+
+        private void btnEnv_Click(object sender, EventArgs e)
+        {
+            //AJUADA-NOS A TRABALHAR COM O SISTEMA (ENVIROMENT = AMBIENTE)
+
+            string meusDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            string dirAtual = Environment.CurrentDirectory;
+
+
+            //lblresultado.Text = dirAtual + Environment.NewLine + meusDocs; //QUEBRA DE LINHA
+
+            //string varAmb = Environment.GetEnvironmentVariable("Path");
+
+            //lblresultado.Text = varAmb;
+
+            //    string[] discos = Environment.GetLogicalDrives();
+
+            //    foreach (string item in discos)
+            //    {
+            //        lblresultado.Text += Environment.NewLine + item  ;
+            //    }
+            //
+
+            string user = Environment.UserName;
+            string dominio = Environment.UserDomainName;
+
+            lblresultado.Text = user + "    " + dominio;
+        
+        
+        }
+
+        private void btnApl_Click(object sender, EventArgs e)
+        {
+            //Application.Exit();
+
+            //Application.Restart();
+
+            string exec = Application.ExecutablePath;
+
+            string pasta = Application.StartupPath;
+
+            lblresultado.Text = exec + "   " + pasta;
+        }
+    }
+}
