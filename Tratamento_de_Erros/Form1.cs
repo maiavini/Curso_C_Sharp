b8b464b93bc580ce9a12f4598d681b9dd3a1bf91
Vinicius Maia <maiavini@gmail.com>
2021-03-10 16:07:46 +0000

    Tratamento de Erro
    Aprendizado Try,Catch,Finally

---
+++ Tratamento_de_Erros/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tratamento_de_Erros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] num = { 10, 20, 30, 40, 50};
+
+            try
+            {
+                string res = "";
+
+                for (int i = 0; i < 15; i++)
+                {
+                    res += num[i] + " ";
+                }
+                label1.Text = res;
+            }
+            catch (Exception ex) //EXIBE O ERRO NATIVO
+            {
+                label1.Text = "ERRO! " + ex.Message ; // EXIBE MENSSAGEM MAIS AMIGÁVEL
+                //throw; FAZ COM QUE A APLICAÃO CONTINUE BLOQUEADA
+            }
+            finally //SEMPRE SERÁ EXECUTADO COM OU SEM ERRO
+            {
+                MessageBox.Show("Operação Finalizada");
+            }
+        }
+    }
+}
